ff2abc6c03730dea70abc8b8678b6174de13dc81
Saif Salem <saifsalemmail@gmail.com>
2024-10-12 14:45:37 +0300

    edit: add the routed in the app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
-function App() {
-  return <h1>Bosta</h1>;
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import ShipmentDetails from "./pages/ShipmentDetailsPage";
+
+export enum RoutesEnum {
+  SHIPMENT_PAGE = "/",
 }
+
+const App: React.FC = () => (
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route path={RoutesEnum.SHIPMENT_PAGE} element={<ShipmentDetails />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
