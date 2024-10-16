aa1aa7790a1c8d54a6c1693bc5d20aa105e5149a
Saif Salem <saifsalemmail@gmail.com>
2024-10-16 06:41:51 +0300

    update: firebase setup

---
+++ firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBBbfBwhtf4fLDFWFJSE2HDZdV1wGQPIT0",
+  authDomain: "bosta-3c9d7.firebaseapp.com",
+  projectId: "bosta-3c9d7",
+  storageBucket: "bosta-3c9d7.appspot.com",
+  messagingSenderId: "358848022455",
+  appId: "1:358848022455:web:bea100acdb96ec9a6d4a32"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
